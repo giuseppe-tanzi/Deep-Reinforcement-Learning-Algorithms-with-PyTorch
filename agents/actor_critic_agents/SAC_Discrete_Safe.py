9ce89030f32b12e3ac47a7f5bd244acb31004c92
Flavio Pinzarrone <flaviop99@live.it>
2023-09-01 17:04:58 +0200

    fixed optimization on larger env

--- agents/actor_critic_agents/SAC_Discrete_Safe.py
+++ agents/actor_critic_agents/SAC_Discrete_Safe.py
@@ -1,5 +1,6 @@
 from agents.actor_critic_agents.SAC_Discrete import SAC_Discrete
 import numpy as np
+import time
 
 class SAC_Discrete_Safe(SAC_Discrete):
     def __init__(self, config):
@@ -28,3 +29,16 @@ class SAC_Discrete_Safe(SAC_Discrete):
                 break
         if safe:
             memory.add_experience(*experience)
+    
+    def run_n_episodes(self, num_episodes=None, show_whether_achieved_goal=True, save_and_print_results=True):
+        """Runs game to completion n times and then summarises results and saves model (if asked to)"""
+        if num_episodes is None: num_episodes = self.config.num_episodes_to_run
+        start = time.time()
+        while self.episode_number < num_episodes:
+            self.reset_game()
+            self.step()
+            if save_and_print_results: self.save_and_print_result()
+        time_taken = time.time() - start
+        if show_whether_achieved_goal: self.show_whether_achieved_goal()
+        if self.config.save_model: self.locally_save_policy()
+        return self.game_full_episode_scores, self.rolling_results, time_taken
